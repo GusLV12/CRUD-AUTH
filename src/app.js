ca9ad32991f9bd1129a873823686592c034116c1
GusLV12 <123591605+GusLV12@users.noreply.github.com>
2023-11-28 19:56:44 -0600

    Actualizacion app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import morgan from 'morgan';
 const app = express();
-const port = 3000;
+
+app.use(morgan('dev'));
  export default app;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import {app, port} from "./app";
-app.listen(port);
+import app from "./app.js";
+const port = 3000;
+app.listen(port)
+console.log("Server listen",port)
